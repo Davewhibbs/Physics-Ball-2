91f60585c2b3fda11aae755ac459ac273e8d97dc
David Winstein-Hibbs <dwinste@wustl.edu>
2020-12-01 08:37:36 -0600

    Adding Pivot Modder, updating controls

--- Assets/BallControl.cs
+++ Assets/BallControl.cs
@@ -9,44 +9,74 @@ public class BallControl : MonoBehaviour
     public Rigidbody2D rb;
     public LineRenderer lr;
     public ParticleSystem releaseParticles;
+    public bool idle = false;
 
     Vector3 dragStartPos;
     Touch touch;
 
+    // Toggles the time scale between 1 and 0.7
+    // whenever the user hits the Fire1 button.
+
+    private float fixedDeltaTime;
+
+
+
+
     public float slowIntensity = .01f; // Between 1-0
+
+    // Start Idle, Set DeltaTime for slowing
+    void Awake()
+    {
+        // Make a copy of the fixedDeltaTime, it defaults to 0.02f, but it can be changed in the editor
+        this.fixedDeltaTime = Time.fixedDeltaTime;
+
+        idle = true;
+    }
 
     private void Update()
     {
+
+        // Touch Input
         if (Input.touchCount > 0) // If there are touchs
         {
             touch = Input.GetTouch(0); // Get input from 1 finger
-
+       
             switch (touch.phase)
             {
                 case TouchPhase.Began:
                     DragStart();
                     break;
-
+       
                 case TouchPhase.Moved:
                     Dragging();
                     break;
-
+       
                 case TouchPhase.Ended:
                     DragRelease();
                     break;
-
+       
                 default:
                     break;
             }
-
+       
         }
+
+        // If Idle, set velocity to 0
+        if (idle)
+        {
+            rb.velocity = new Vector3(0, 0, 0);
+        }
+        
     }
 
+    // States of Touch Controls
     void DragStart()
     {
+        // Get Drag start position
         dragStartPos = Camera.main.ScreenToWorldPoint(touch.position);
         dragStartPos.z = 0f; // Stop 3D dragging
 
+        // Get Line Renderer start position
         lr.positionCount = 1;
         lr.SetPosition(0, dragStartPos);
 
@@ -54,38 +84,47 @@ public class BallControl : MonoBehaviour
 
     void Dragging()
     {
+
         Vector3 draggingPos = Camera.main.ScreenToWorldPoint(touch.position);
         draggingPos.z = 0f;
         lr.positionCount = 2;
         lr.SetPosition(1, draggingPos);
 
         // Slow Time
-        // This goes in Dragging
         Time.timeScale = slowIntensity;
 
         // Adjust fixed delta time according to timescale
         // The fixed delta time will now be 0.02 frames per real-time second
         Time.fixedDeltaTime = this.fixedDeltaTime * Time.timeScale;
-
-        Debug.Log(Time.timeScale);
     }
 
     void DragRelease()
     {
+        // If idle, no longer
+        if (idle) idle = false;
+
         // Resume normal time
         Time.timeScale = 1.0f;
 
+        // Delete lineRenderer line
         lr.positionCount = 0;
 
+        // Get Last touch position
         Vector3 dragReleasePos = Camera.main.ScreenToWorldPoint(touch.position);
         dragReleasePos.z = 0f;
 
+        // Calculate clamped Force to apply to Rigidbody
         Vector3 force = dragStartPos - dragReleasePos;
         Vector3 clampedForce = Vector3.ClampMagnitude(force, maxDrag) * power;
 
+        // Reset Velocity
+        rb.velocity = new Vector3(0f, 0f, 0f);
+
+        // Add the Force
         rb.AddForce(clampedForce, ForceMode2D.Impulse);
 
-        
+
+        //releaseParticles.transform.position = dragStartPos;
         releaseParticles.Play();
 
 
@@ -113,6 +152,7 @@ public class BallControl : MonoBehaviour
         
     }
 
+    // Colliding with RigidBodies
     private void OnCollisionEnter2D(Collision2D collision)
     {
         // Choose random Bounce sound
@@ -131,16 +171,8 @@ public class BallControl : MonoBehaviour
     }
 
 
-    // Toggles the time scale between 1 and 0.7
-    // whenever the user hits the Fire1 button.
-
-    private float fixedDeltaTime;
-
-    void Awake()
-    {
-        // Make a copy of the fixedDeltaTime, it defaults to 0.02f, but it can be changed in the editor
-        this.fixedDeltaTime = Time.fixedDeltaTime;
-    }
+    
+    
     
     
         
